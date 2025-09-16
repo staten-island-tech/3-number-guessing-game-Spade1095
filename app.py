86b8a9bfdcc0752f3ef24d24e37da9bbbcc05d58
Spade1095 <henrym181@nycstudents.net>
2025-09-16 06:48:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import math
-num = math.random(1,10)
+import random
+num = random.randint(1, 10)
+correct = False
+numOfGuess = 0
+guess = 0
+while correct == False:
+    guess = int(input("What is your guess"))
+    numOfGuess = numOfGuess + 1
+    if guess == num:
+        print("Thats correct!")
+        print(f"You took {numOfGuess} trys to guess!")
+        correct = True
+    elif guess > num:
+        print(f"{guess} is too big!")
+    else:
+        print(f"{guess} is too small!")
