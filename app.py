41b52a6c44fde7a8ad7e532a2bf8c76cb4657264
Spade1095 <henrym181@nycstudents.net>
2025-10-16 06:44:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-import random
-range = int(input("What range do you want to guess100 from? "))
-num = random.randint(1, range)
-correct = False
-numOfGuess = 0
-guess = 0
-while correct == False:
-    guess = int(input("What is your guess "))
-    numOfGuess = numOfGuess + 1
-    if guess == num:
-        print("Thats correct!")
-        print(f"You took {numOfGuess} trys to guess!")
-        correct = True
-    elif guess > num:
-        print(f"{guess} is too big!")
-    else:
-        print(f"{guess} is too small!")
+# import random
+# range = int(input("What range do you want to guess100 from? "))
+# num = random.randint(1, range)
+# correct = False
+# numOfGuess = 0
+# guess = 0
+# while correct == False:
+#     guess = int(input("What is your guess "))
+#     numOfGuess = numOfGuess + 1
+#     if guess == num:
+#         print("Thats correct!")
+#         print(f"You took {numOfGuess} trys to guess!")
+#         correct = True
+#     elif guess > num:
+#         print(f"{guess} is too big!")
+#     else:
+#         print(f"{guess} is too small!")
+
+
+int = 1
+float = 1.0
+string = "your dumb"
+list = [ 0,0,0,0,"this is your grades stupid"]
+bolean = False
+print(list)
